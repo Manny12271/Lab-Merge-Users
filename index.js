d8b62237293c7588cdddce69f1f9ad76283d2a41
manuel alfaro <manuel.alfaro247@gmail.com>
2025-05-16 19:03:46 -0500

    username merger

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+require('datejs');
 
+function combineUsers(...args) {
+  // Step 2: Initialize the return object
+  const combinedObject = {
+    users: [],
+  };
 
+  // Step 3 & 4: Loop through args and merge all arrays into users
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  // Step 5: Get today's date in M/d/yyyy format
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  // Step 7: Return the combined object
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
